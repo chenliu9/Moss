4c95c576d180ccc83f8db58d0accd8a057bd0526
Chen Liu <chen@txz.tech>
2024-05-14 23:45:16 +0800

    WXShare (#7)

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -2,6 +2,8 @@ import { notFound } from 'next/navigation'
 import { Chat } from '@/components/chat'
 import { getSharedChat } from '@/lib/actions/chat'
 import { AI } from '@/app/actions'
+import WeixinShareWrapper from '@/components/weixin-share-wrapper'
+import { headers } from 'next/headers'
 
 export interface SharePageProps {
   params: {
@@ -51,6 +53,9 @@ export default async function SharePage({ params }: SharePageProps) {
     notFound()
   }
 
+  const headersList = headers()
+  const domain = headersList.get('host') || ''
+
   return (
     <AI
       initialAIState={{
@@ -59,6 +64,18 @@ export default async function SharePage({ params }: SharePageProps) {
         isSharePage: true
       }}
     >
+      <WeixinShareWrapper
+        url={domain + '/share/' + params.id}
+        title={
+          chat?.title.toString().slice(0, 50) ||
+          'Moss - 动态、复杂、高维数据的智能分析'
+        }
+        desc={
+          chat?.messages[0].content ||
+          '基于先进的数据+AI一体化引擎，赋能企业对运营生产动态的实时监控与掌握，实现生产经营的降本增益！'
+        }
+        imgUrl={domain + '/opengraph-image.png'}
+      />
       <Chat id={params.id} />
     </AI>
   )

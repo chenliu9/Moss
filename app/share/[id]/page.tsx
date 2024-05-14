b0b6be83a6b82370f2ce0a6c416215b7cc24a438
Chen Liu <chen@txz.tech>
2024-05-15 00:14:10 +0800

    update metadata desc

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -22,7 +22,6 @@ export async function generateMetadata({ params }: SharePageProps) {
     'Moss - 动态、复杂、高维数据的智能分析'
 
   const description =
-    chat.messages[0].content ||
     '基于先进的数据+AI一体化引擎，赋能企业对运营生产动态的实时监控与掌握，实现生产经营的降本增益！'
 
   return {

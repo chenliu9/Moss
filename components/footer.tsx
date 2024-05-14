3a42dc44c72b4d95c410c7e884714e05a86187da
Chen Liu <chen@txz.tech>
2024-05-14 22:23:13 +0800

    Update from upstream (#5)
    * UI changes
    
    * update package name and readme
    
    * update footer
    
    * update metadata on search page
    
    * update desc
    
    * Miurla main (#2)
    
    * Add ClearHistoryButton component and dependencies
    
    * Add Toaster component and clear history functionality
    
    * Update clear history confirmation message
    
    * fix history: chat maybe undefined
    
    * Update example messages in empty-screen component
    
    * UI changes
    
    * update package name and readme
    
    * update footer
    
    * update metadata on search page
    
    * update desc
    
    ---------
    
    Co-authored-by: Yoshiki Miura <miurap400@gmail.com>
    Co-authored-by: Baorong Li <superlbr@126.com>
    
    * UI changes
    
    * update package name and readme
    
    * update footer
    
    * update metadata on search page
    
    * update desc
    
    * update from upstream
    
    * cleanup
    
    ---------
    
    Co-authored-by: Yoshiki Miura <miurap400@gmail.com>
    Co-authored-by: Baorong Li <superlbr@126.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
-import { Button } from './ui/button'
+// import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
+// import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
-        <Button
+        ©{new Date().getFullYear()}
+        {/* <span className="pl-1">txz.tech</span> */}
+        <Link href="https://txz.tech" target="_blank" className="pl-1">
+          txz.tech
+        </Link>
+        {/* <Button
           variant={'ghost'}
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://twitter.com/morphic_ai" target="_blank">
+          <Link href="https://twitter.com/miiura" target="_blank">
             <SiTwitter size={18} />
           </Link>
         </Button>
@@ -33,7 +29,7 @@ const Footer: React.FC = () => {
           <Link href="https://git.new/morphic" target="_blank">
             <SiGithub size={18} />
           </Link>
-        </Button>
+        </Button> */}
       </div>
     </footer>
   )
